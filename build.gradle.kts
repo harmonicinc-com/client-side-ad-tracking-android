1bf86396ca17125f58ded8a28452c48ec1f05695
Michael Leung <michael.leung@harmonicinc.com>
2025-07-31 15:48:45 +0800

    ci: migrate to Central Publisher Portal for publishing to maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.android.application") version "8.1.2" apply false
     id("com.android.library") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
-    id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
+    id("com.vanniktech.maven.publish") version "0.30.0" apply false
 }
 
 allprojects {
@@ -11,26 +11,5 @@ allprojects {
         google()
         mavenCentral()
         gradlePluginPortal()
-    }
-}
-
-/*
-    Nexus publishing config - Must be in root
- */
-
-val sonatypeStagingProfileId: String? = System.getenv("SONATYPE_STAGING_PROFILE_ID")
-val ossrhUsername: String? = System.getenv("OSSRH_USERNAME")
-val ossrhPassword: String? = System.getenv("OSSRH_PASSWORD")
-
-nexusPublishing {
-    packageGroup.set("com.github.harmonicinc-com")
-    repositories {
-        sonatype {
-            stagingProfileId.set(sonatypeStagingProfileId)
-            username.set(ossrhUsername)
-            password.set(ossrhPassword)
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-        }
     }
 }
